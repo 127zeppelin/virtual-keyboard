eaa9e13a28bb5d12066df9eaf384dc6098cf2152
127zeppelin <127.zeppelin@gmail.com>
2023-04-30 01:09:13 +0300

    feat: add virtual keyboard, keys, and text field

---
+++ components/keys.js
@@ -0,0 +1,35 @@
+const KEYBOARD = document.createElement('div');
+KEYBOARD.className = 'container__keyboard keyboard';
+export const CONTAINER = document.createElement('div');
+CONTAINER.className = 'container';
+document.body.appendChild(CONTAINER);
+CONTAINER.appendChild(KEYBOARD);
+
+
+async function getDataKeys() {
+  const url = './components/key-values.json';
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function main() {
+  const dataKeys = await getDataKeys();
+  console.log(dataKeys);
+  for(let i = 0; dataKeys.length > i; i++){
+    const KEY_BTN = document.createElement('button');
+    KEY_BTN.className = `keyboard__key ${dataKeys[i].className}`;
+    KEYBOARD.appendChild(KEY_BTN);
+    const KEY_TEXT = document.createElement('span');
+    KEY_BTN.appendChild(KEY_TEXT);
+    KEY_TEXT.innerHTML = `${dataKeys[i].eng.lowerCase}`
+  }
+}
+
+
+
+export {main};
---
+++ script.js
@@ -0,0 +1,14 @@
+import { CONTAINER } from "./components/keys.js";
+import { main } from './components/keys.js';
+
+
+const TITLE = document.createElement('h1');
+TITLE.className = 'container__title';
+TITLE.innerHTML = '_RS Virtual keybord';
+CONTAINER.insertBefore(TITLE, CONTAINER.firstChild);
+const TEXT_AREA = document.createElement('textarea');
+TEXT_AREA.className = 'container__text';
+TEXT_AREA.setAttribute('rows', '7');
+CONTAINER.insertBefore(TEXT_AREA, TITLE.nextSibling);
+
+main();
