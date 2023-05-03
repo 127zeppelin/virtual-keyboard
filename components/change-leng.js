fa5523d1ebcb092e7e087b90a2dec686d01e36ce
127zeppelin <127.zeppelin@gmail.com>
2023-05-03 04:34:38 +0300

    fix: Caps Lock button functionality

--- components/change-leng.js
+++ components/change-leng.js
@@ -1,32 +1,46 @@
+import {pressedCaps} from './press-caps.js';
 export const ChangeLeng = (KEYBOARD_KEYS) => {
-
+ 
   let pressedKeys = new Set();
-    
+  let currentLayout = 'eng-'+ pressedCaps;
   const changeLanguage = () => {
-    for(let i = 0; i < KEYBOARD_KEYS.length; i++){
-    if (KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.contains('current')) {
-        KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.remove('current');
-        KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.add('current');
-      } else if  (KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.contains('current')) {
-        KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.remove('current');
-        KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.add('current');
-      } else if  (KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.contains('current')) {
-        KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.remove('current');
-        KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.add('current');
-      } else if  (KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.contains('current')) {
-        KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.add('current');
-        KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.remove('current');
-      }
     
-    } 
+    console.log(currentLayout); 
+    for (let i = 0; i < KEYBOARD_KEYS.length; i++) {
+      switch (true) {
+        case KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.contains('current'):
+          KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.remove('current');
+          KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.add('current');
+          currentLayout = 'ru-'+ pressedCaps;
+          break;
+        case KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.contains('current'):
+          KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.add('current');
+          KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.remove('current');
+          currentLayout = 'eng-'+ pressedCaps;
+          break;
+        case KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.contains('current'):
+          KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.remove('current');
+          KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.add('current');
+          currentLayout = 'ru-'+ pressedCaps;;
+          break;
+        case KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.contains('current'):
+          KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.remove('current');
+          KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.add('current');
+          currentLayout = 'eng-'+ pressedCaps;
+          break;
+        default:
+          break;
+      }
+    }
+    
   };
-  
+  localStorage.setItem('keyboardLayout', currentLayout);
   document.addEventListener('keydown', (event) => {
     pressedKeys.add(event.code);
 
     if (pressedKeys.has('ShiftLeft') && pressedKeys.has('AltLeft')) {
       changeLanguage();
-    } else if (pressedKeys.has('ShiftRight') && pressedKeys.has('AltRight')){
+    } else if (pressedKeys.has('ShiftRight') && pressedKeys.has('AltRight')) {
       changeLanguage();
     }
   });
@@ -35,3 +49,4 @@ export const ChangeLeng = (KEYBOARD_KEYS) => {
     pressedKeys.delete(event.code);
   });
 };
+
--- components/press-caps.js
+++ components/press-caps.js
@@ -1,61 +1,41 @@
+let pressedCaps = 'down';
 export const CapsPresPsih = (KEYBOARD_KEYS) => {
-  
+  const CAPS_LOCK = document.querySelector('.CapsLock');
   const changeCaseCaps = () => {
-    for(let i = 0; i < KEYBOARD_KEYS.length; i++){
-    if (KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
+    for (let i = 0; i < KEYBOARD_KEYS.length; i++) {
+      if (KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
         KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.remove('current');
         KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.add('current');
-      } else if  (KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
+      } else if (KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
         KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.remove('current');
         KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.add('current');
-      } else if  (KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
+      } else if (KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
         KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.remove('current');
         KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.add('current');
-      } else if  (KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
+      } else if (KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
         KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.add('current');
         KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.remove('current');
       }
-    } 
+    }
   };
- 
-  
+
+
   document.addEventListener('keydown', (event) => {
     if (event.code === 'CapsLock') {
-        changeCaseCaps();      
+      pressedCaps = (pressedCaps === 'down') ? 'up' : 'down';
+      CAPS_LOCK.classList.toggle('active');
+      changeCaseCaps();
+      console.log(pressedCaps);
     }
   });
-};
-
-
-export const CapsPresVirtual = (KEYBOARD_KEYS) => {
-  const CAPS_LOCK = document.querySelector('.CapsLock');
-  const changeCase = () => {
-    for(let i = 0; i < KEYBOARD_KEYS.length; i++){
-      if (KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
-          KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.remove('current');
-          KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.add('current');
-        } else if  (KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
-          KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.remove('current');
-          KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.add('current');
-        } else if  (KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
-          KEYBOARD_KEYS[i].querySelector('span.eng-up').classList.remove('current');
-          KEYBOARD_KEYS[i].querySelector('span.eng-doun').classList.add('current');
-        } else if  (KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.contains('current') && !KEYBOARD_KEYS[i].classList.contains('caps')) {
-          KEYBOARD_KEYS[i].querySelector('span.ru-doun').classList.add('current');
-          KEYBOARD_KEYS[i].querySelector('span.ru-up').classList.remove('current');
-        }
-      } 
-  };
-
-  CAPS_LOCK.addEventListener('mousemove', (event) => {
-    CAPS_LOCK.focus();
-});
 
   CAPS_LOCK.addEventListener('mousedown', (event) => {
-      changeCase();
-      CAPS_LOCK.classList.toggle('active');
+    changeCaseCaps();
+    pressedCaps = (pressedCaps === 'down') ? 'up' : 'down';
+    console.log(pressedCaps);
+    CAPS_LOCK.classList.toggle('active');
   });
-
-
-
 };
+
+
+export { pressedCaps };
